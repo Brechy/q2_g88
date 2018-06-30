6b9e7ff07b4d36548d9577dd1550863fba346b4a
NikSS <nik.safe@gmail.com>
2018-06-30 16:54:20 -0700

    added categories_id to the offer table

--- migrations/20180630160251_offers.js
+++ migrations/20180630160251_offers.js
@@ -3,6 +3,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('offers', (table) => {
     table.increments();
     table.integer('users_id').notNullable().references('users.id');
+    table.integer('categories_id').notNullable().references('categories.id');
     table.timestamps(true, true)
   })
 }
