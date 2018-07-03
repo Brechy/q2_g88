c1f2988bdb16ffcbb9d96c2a2a32d1d0e289dabc
JBallin <JBallin@users.noreply.github.com>
2018-07-02 17:47:14 -0700

    Clean up users migration
    - fix instagram typo
    - make some fields nullable
    - reorder fields

--- migrations/20180630155901_users.js
+++ migrations/20180630155901_users.js
@@ -3,13 +3,13 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', (table) => {
     table.increments()
     table.string('name').notNullable()
-    table.text('bio').notNullable()
-    table.specificType('hashed_password', 'char(60)').notNullable();
     table.string('email').unique().notNullable();
-    table.string('facebook').unique().notNullable();
-    table.string('instagaram').unique().notNullable();
+    table.specificType('hashed_password', 'char(60)').notNullable();
     table.string('city').notNullable();
+    table.text('bio').notNullable();
     table.text('image_url').notNullable();
+    table.string('facebook').unique();
+    table.string('instagram').unique();
     table.timestamps(true, true)
   })
 }
