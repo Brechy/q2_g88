343c385c1c29278bd6a1e1f987d3aed9c5de8e2d
NikSS <nik.safe@gmail.com>
2018-07-05 13:30:52 -0700

    app.POST route

--- public/javascripts/signupform.js
+++ public/javascripts/signupform.js
@@ -1,11 +1,30 @@
-console.log("hello");
+console.log("hellooo");
+// let signup = document.querySelector('#signup');
+// console.log(signup);
+document.addEventListener("DOMContentLoaded", (event) => {
+  let signup = document.querySelector('#signup');
+  console.log(signup,"<<<<<signup2");
+    console.log("DOM fully loaded and parsed")
+    signup.addEventListener("click",(event) => {
+      
+    console.log("DOM fully loaded and parsed")
+    console.log("i am clicking you mr signup2");
+          fetch('/signup', {
+            method: 'POST',
+            headers:{
+            'Content-Type':'application/json'
+          },
+          body: JSON.stringify(info)
+          })
+          .then((response) => {
+            return response.json()
+          })
+          .then((myUsers) => {
+            console.log(myUsers)
+          })
 
-document.addEventListener("DOMContentLoaded", function(event) {
 
-        console.log("DOM fully loaded and parsed")
-        fetch('/signup', { method: 'GET'})
-        .then((response) => {
-          return response.json()
-        })
-        .then((myUsers) => {
-          console.log(myUsers)
+
+  })
+
+})
