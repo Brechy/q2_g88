b46a204ef5e0f56378c35b1563d27a932f3ec617
JBallin <JBallin@users.noreply.github.com>
2018-07-06 08:34:41 -0700

    Begin writing users test

---
+++ test/users.test.js
@@ -0,0 +1,12 @@
+const request = require('supertest');
+const app = require('../app.js');
+const knex = require('../knex');
+
+knex.migrate.rollback()
+.then(() => knex.migrate.latest())
+.then(() => knex.seed.run())
+.then(() => knex.select().from('users'))
+.then(data => console.log(data))
+.then(() => {})
+
+// describe('should be')
