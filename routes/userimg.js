f1f2f52a76073aabe86ea81e69b7e90697c8fd41
Corina Andariese <candariese@gmail.com>
2018-07-06 11:39:18 -0700

    My bad, fixing stashed changes from master to correct branch.

--- routes/userimg.js
+++ routes/userimg.js
@@ -3,11 +3,48 @@ const router = express.Router();
 const knex = require('../knex');
 const path = require('path');
 
+<<<<<<< Updated upstream
 //path to html file
 const imgUpload = path.join(__dirname, '..', 'public/user_img.html');
 
 router.get('/', (req, res, next) => {
 	res.sendFile(imgUpload);
+=======
+const userImage = path.join(__dirname, '..', 'public/user_img.html');
+
+router.get('/', (req, res, next) => {
+	res.sendFile(userImage);
+});
+
+//Upload an image to database
+
+router.post('/', (req, res, next) => {
+	if(!req.body.img) {
+		res.sendStatus(403);
+	}
+	let img = req.body.image;
+
+	//check if that image link is already in the database
+	knex('users')
+		.where('image', req.body.image)
+		.first()
+		.then((user) => {
+			if(user) {
+				throw new Error('That image has already been uploaded');
+			}
+		})
+		.returning('*')
+		.then((result) => {
+			res.direct('Sucess!', result);
+			//send user to next step upon sucessful upload
+			res.redirect('/signup2');
+		})
+		.catch((err) => {
+			next(err);
+			//can be handled in the error handler file
+			res.status('401').send({error:'Error, refresh page'});
+		});
+>>>>>>> Stashed changes
 });
 
 module.exports = router;
