6928e1d6410ac59df00289a69a35ab82809c9769
JBallin <JBallin@users.noreply.github.com>
2018-07-08 10:51:57 -0700

    Clean app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('../app');
 
 describe('App', function() {
-  it('should respond with status 200', (done) => {
+  it('should respond with status 200', done => {
     request(app)
       .get('/')
       .expect(200, done);
