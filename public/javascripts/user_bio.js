62ca09b4f224e96a8050db771f7b77bebdff9a6e
NikSS <nik.safe@gmail.com>
2018-07-09 12:49:25 -0700

    remove city validation from user-bio.js

--- public/javascripts/user_bio.js
+++ public/javascripts/user_bio.js
@@ -1,13 +1,1 @@
 
-document.addEventListener("DOMContentLoaded", (event) => {
-    addEventListeners()
-    })
-
-function Validation() {
-  var txt = document.querySelector("#city")
-    if (txt.value !== 'San Francisc'){
-      alert("Invalid city name");
-    }  else{
-        alert('Valid city name');
-          return true;
-    }
