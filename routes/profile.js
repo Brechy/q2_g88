070c847a6017ba24f1786d62bebf13f05ce4b0f7
NikSS <nik.safe@gmail.com>
2018-07-12 18:10:36 -0700

    add profile route

--- routes/profile.js
+++ routes/profile.js
@@ -9,4 +9,15 @@ router.get('/', (req, res, next) => {
 	res.sendFile(profiltHTML);
 });
 
+router.get('/', (req, res, next) => {
+  // USE KNEX TO GET A SPECIFIC USER
+  knex('users')
+  .where('id', req.params.userid)
+  .then((data) => {
+    console.log('the specific user', data)
+    res.send(data)
+  })
+})
+
+
 module.exports = router;
