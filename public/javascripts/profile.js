7861f0e7a2ab2828252f47b0e00eec4608da04e3
NikSS <nik.safe@gmail.com>
2018-07-13 17:28:30 -0700

    write /profile, /profile:userid route
    Co-authored-by: JBallin <JBallin@users.noreply.github.com>

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -1,40 +1,60 @@
-//DOM loaded and starts looking for buttons
-	console.log("i am here do you see me");
-document.addEventListener('DOMContentLoaded', (event) => {
+const id = window.location.pathname.replace('/profile', '').replace('/', '') || window.localStorage.getItem('fam:id');
+console.log(id,"<<<id");
 
-  userId();
-	usersData();
+const name = document.querySelector("#username");
+const bio = document.querySelector("#bio");
+const offers = document.querySelector("#offers");
+const requests = document.querySelector("#requests");
 
-});
+fetch('/profile/api/' + id)
+.then(res => res.json())
+.then(user => {
+	name.innerText = user[0].name
+	bio.innerText = user[0].bio
+	// offers
+	// for (offer of offers)
+	// requests
+})
 
-const userId = () =>{
-
-	 const id = window.localStorage.getItem('fam:id');
-   console.log(id,"<<<<<id");
-   return id;
-}
 
 
-
-
-const usersData = async() => {
-try{
-console.log('i am here');
-	let username = document.querySelector("#username").innerText;
-	let bio = document.querySelector("#bio").innerText;
-	let offer = document.querySelector("#offers").innerText;
-	let request = document.querySelector("#requests").innertext;
-
-    let data = await fetch('/profile/api/:id',{method :'GET'})
-    .then((response) => {
-         return response.json();
-    }).then((data)=> {
-			console.log(data,"<<<data");
-			 // username = data.name;
-			 // bio = data.bio
-
-		})
-	} catch (err) {
-    console.log(err);
-  }
-  }
+//
+// //DOM loaded and starts looking for buttons
+// console.log("i am here do you see me");
+// document.addEventListener('DOMContentLoaded', (event) => {
+//
+//   userId();
+// 	usersData();
+//
+// });
+//
+// const userId = () =>{
+// 	 const id = window.localStorage.getItem('fam:id');
+//    console.log(id,"<<<<<id");
+//    return id;
+// }
+//
+//
+//
+//
+// const usersData = async() => {
+// try{
+// console.log('i am here');
+// 	let username = document.querySelector("#username").innerText;
+// 	let bio = document.querySelector("#bio").innerText;
+// 	let offer = document.querySelector("#offers").innerText;
+// 	let request = document.querySelector("#requests").innertext;
+//
+//     let data = await fetch('/profile/api/:id',{method :'GET'})
+//     .then((response) => {
+//          return response.json();
+//     }).then((data)=> {
+// 			console.log(data,"<<<data");
+// 			 // username = data.name;
+// 			 // bio = data.bio
+//
+// 		})
+// 	} catch (err) {
+//     console.log(err);
+//   }
+//   }
--- routes/profile.js
+++ routes/profile.js
@@ -6,20 +6,25 @@ const knex = require('../knex')
 const profileHTML = path.join(__dirname, '..', 'public/profile.html');
 const myProfileHTML = path.join(__dirname, '..', 'public/myprofile.html');
 
-router.get('/view/:userid', (req, res, next) => {
+router.get('/', (req, res, next) => {
 	res.sendFile(profileHTML);
 });
 
-// this route is giving me my profile
-router.get('/', (req, res, next) => {
-	res.sendFile(myProfileHTML)
-})
+router.get('/:userid', (req, res, next) => {
+	res.sendFile(profileHTML);
+});
 
-//this route is for getting one user.
+
+// // this route is giving me my profile
+// router.get('/', (req, res, next) => {
+// 	res.sendFile(myProfileHTML)
+// })
+
+//this route is for getting one user as JSON.
 router.get('/api/:userid', (req, res, next) => {
   // USE KNEX TO GET A SPECIFIC USER
   knex('users')
-  .where('id', req.params.id)
+  .where('id', req.params.userid)
   .then((data) => {
     console.log('the specific user', data)
     res.json(data)
