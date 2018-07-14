cdc187ec5a0e56485d2839071ee62e299238cdf1
NikSS <nik.safe@gmail.com>
2018-07-14 11:01:12 -0700

    fix facebook.unique in the migration to save bio in DB

---
+++ routes/offers.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const knex = require('../knex')
+
+router.get('/:userid', (req, res, next) => {
+	knex
+	// SELECT offers.id, categories.title
+	.select('offers.id', 'categories.title')
+	// FROM offers
+	.from('offers')
+	// INNER JOIN categories
+	// ON offers.categories_id=categories.id
+	.innerJoin('categories', 'offers.categories_id', 'categories.id')
+	// WHERE offers.users_id=2;
+	.where('offers.users_id', req.params.userid)
+	.then(data => res.json(data));
+})
+
+module.exports = router;
