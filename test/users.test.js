fd615f70ed77d8f7ddc1a15ea3085ad11437fced
JBallin <JBallin@users.noreply.github.com>
2018-07-14 11:27:39 -0700

    Merge from profile branch

--- test/users.test.js
+++ test/users.test.js
@@ -4,13 +4,16 @@ const request = require('supertest');
 const app = require('../app');
 const knex = require('../knex');
 
-knex.migrate.rollback()
-.then(() => knex.migrate.latest())
-.then(() => knex.seed.run())
-.then(() => knex.select().from('users'))
-.finally(() => knex.destroy());
 
 describe('GET /users', () => {
+  before(() => {
+    return knex.migrate.rollback()
+    .then(() => knex.migrate.latest())
+    .then(() => knex.seed.run())
+    .then(() => knex.select().from('users'))
+  })
+  after(() => knex.destroy());
+
   it('should return JSON', done => {
     request(app)
     .get('/users')
