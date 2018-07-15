c53bdbe0ee2fef40ce5afb912878828d6d01eb0a
JBallin <JBallin@users.noreply.github.com>
2018-07-15 16:30:31 -0700

    Send members HTML file for GET /members

--- routes/members.js
+++ routes/members.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+
+const membersPage = path.join(__dirname, '..', 'public/members.html')
+
+router.get('/', (req, res) => res.sendFile(membersPage))
 
 module.exports = router;
