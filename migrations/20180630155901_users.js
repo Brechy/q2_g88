45c86e2fad0061fccdf3f57a063d9a34a9db8bc7
JBallin <JBallin@users.noreply.github.com>
2018-07-14 21:44:38 -0700

    Make name and email notNullable

--- migrations/20180630155901_users.js
+++ migrations/20180630155901_users.js
@@ -2,15 +2,15 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', (table) => {
     table.increments()
-    table.string('name');
-    table.string('email').unique();
+    table.string('name').notNullable();
+    table.string('email').unique().notNullable();
     table.specificType('hashed_password', 'char(60)');
     table.string('city');
     table.text('bio');
-    table.text('image_url')
+    table.text('image_url');
     table.string('facebook');
     table.string('instagram');
-    table.timestamps(true, true)
+    table.timestamps(true, true);
   })
 }
 
