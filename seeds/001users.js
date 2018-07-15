2bc1b6bfff0464248cd569ec479939bd34dd5710
JBallin <JBallin@users.noreply.github.com>
2018-07-15 00:42:20 -0700

    Fix seeded users social URL's

--- seeds/001users.js
+++ seeds/001users.js
@@ -9,8 +9,8 @@ exports.seed = function(knex, Promise) {
 				city: 'Emeryville',
 				bio: 'Mom of 3, love video games, food, and staring into the sun.',
 				image_url: 'https://www.licenseglobal.com/sites/default/files/images/PusheenMoreLicensees.jpg',
-				facebook: '/tedo_thynia',
-				instagram: '@tedo_coolmom'
+				facebook: 'https://facebook.com/tedo_thynia',
+				instagram: 'https://instagram.com/tedo_coolmom'
 			}, {
 				name: 'Barb',
 				email: 'pizza@eater.com',
@@ -18,8 +18,8 @@ exports.seed = function(knex, Promise) {
 				city: 'Bloopyly',
 				bio: 'Barb Kellner, the Pizza Eater. I run a pizza eating business where we dispose of your leftover pizza by eating it. I have 18 fur-babies.',
 				image_url: 'https://i.pinimg.com/originals/b2/31/ce/b231ce21f00379d9ff9c2f0a9218bda2.jpg',
-				facebook: '/barbkellner',
-				instagram: '@dapizzaeater'
+				facebook: 'https://facebook.com/barbkellner',
+				instagram: 'https://instagram.com/dapizzaeater'
 			}, {
 				name: 'Jimbo',
 				email: 'jimbo@sendhelp.com',
@@ -27,8 +27,8 @@ exports.seed = function(knex, Promise) {
 				city: 'Anywhoodle',
 				bio: 'Work From Home Dad, go WFHDs!! Yeah!! Dad of quintuplets, send help.',
 				image_url: 'https://i.pinimg.com/originals/e2/10/59/e21059c6831270c970e3aa4b576b4faf.gif',
-				facebook: '/jimbocooldad',
-				instagram: '@jimbo_cooldad'
+				facebook: 'https://facebook.com/jimbocooldad',
+				instagram: 'https://instagram.com/jimbo_cooldad'
 			}]);
 		});
 };
