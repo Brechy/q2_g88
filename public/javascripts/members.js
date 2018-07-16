c6e5cbf69d27c7cb8f1f649c37d8ca55a8372e5c
JBallin <JBallin@users.noreply.github.com>
2018-07-15 19:06:49 -0700

    Select members dom elems

---
+++ public/javascripts/members.js
@@ -0,0 +1,24 @@
+// SELECT DOM ELEMENTS
+
+const img1 = document.querySelectorAll('.user-image1');
+const img2 = document.querySelectorAll('.user-image2');
+const img3 = document.querySelectorAll('.user-image3');
+const imgs = [img1, img2, img3];
+
+const name1 = document.querySelectorAll('.username1');
+const name2 = document.querySelectorAll('.username2');
+const name3 = document.querySelectorAll('.username3');
+const names = [name1, name2, name3];
+
+const city1 = document.querySelector('#city1');
+const city2 = document.querySelector('#city2');
+const city3 = document.querySelector('#city3');
+const cities = [city1, city2, city3];
+
+const bio1 = document.querySelector('#bio1');
+const bio2 = document.querySelector('#bio2');
+const bio3 = document.querySelector('#bio3');
+const bios = [bio1, bio2, bio3];
+
+let prev = document.querySelector('#prev');
+let next = document.querySelector('#next');
