7598a613f48af32cd7d329b238f70b4545b1949b
JBallin <JBallin@users.noreply.github.com>
2018-07-15 19:14:25 -0700

    Clean up users route

--- routes/users.js
+++ routes/users.js
@@ -2,15 +2,14 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../knex');
 
-/* GET users listing. */
+// GET All Users
 router.get('/', (req, res, next) =>
 	knex('users')
 	.then(rows => res.json(rows))
 );
 
-//this route is for getting one user as JSON.
+// GET Specific User
 router.get('/:userid', (req, res, next) => {
-  // USE KNEX TO GET A SPECIFIC USER
   knex('users')
   .where('id', req.params.userid)
   .then((data) => {
