f8af082d053e81be752d383e3ee14a9f01b0117e
Corina Andariese <candariese@gmail.com>
2018-07-16 22:27:36 -0700

    Update user_bio to use email-based key from local storage

--- public/javascripts/user_bio.js
+++ public/javascripts/user_bio.js
@@ -1,40 +1,72 @@
 
 document.addEventListener('DOMContentLoaded', (event) => {
-
   addEventListeners();
+  updateBio();
 })
 
 
 
 const addEventListeners = () => {
-  event.preventDefault();
   const submit = document.querySelector("#submit");
-    submit.addEventListener('click',(event) => {
-    userBio ();
-    })
+  submit.addEventListener('click', (event) => {
+    event.preventDefault();
+    sendBio();
+  })
 };
- const userEmail = () =>{
-   	const email = window.localStorage.getItem('fam:email');
-    console.log(email,"<<<<<email");
-    return email;
- }
 
-const userBio = async () => {
+const updateBio = () =>{
+    const email = window.localStorage.getItem('fam:email');
+    if (!email) {
+      window.location.replace('/signup');
+      return;
+    }
+    console.log(email,"<<<<<email");
+
+    fetch('/signup2/'+email)
+    .then((data) => {
+      return data.json();
+    }).then((json) => {
+      if (json.result != 'ok') {
+        console.log("user doesn't exist yet");
+        return;
+      }
+      const bio = document.querySelector("#bio");
+      const city = document.querySelector("#city");
+      const facebook = document.querySelector("#facebook");
+      const instagaram = document.querySelector("#instagram");
+      bio.value = json.bio;
+      city.value = json.city;
+      facebook.value = json.facebook;
+      instagram.value = json.instagram;
+    })
+}
+
+const sendBio = async () => {
+  const email = window.localStorage.getItem('fam:email');
+  if (!email) {
+    window.location.replace('/signup');
+    return;
+  }
+
   try {
     const bio = document.querySelector("#bio").value;
     const city = document.querySelector("#city").value;
     const facebook = document.querySelector("#facebook").value;
     const instagaram = document.querySelector("#instagram").value;
 
-    let data = await fetch('/signup2', {
-      method: 'POST',
+    let data = await fetch('/signup2/'+email, {
+      method: 'PUT',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({ bio, city, facebook, instagaram })
     })
     const response = await data.json()
-    return response;
+    if (response.result != 'ok') {
+      alert("Failed to update bio:\n" + response.message);
+    } else {
+      window.location.replace('/signup3');
+    }
   } catch (err) {
     console.log(err);
   }
